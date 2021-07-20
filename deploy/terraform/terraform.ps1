50670a46325e3279bbc6bd528d1cafaca8a66c0b
Lee Borlace <lee.borlace@gmail.com>
2021-07-20 19:56:51 +0800

    terraform apply

--- deploy/terraform/terraform.ps1
+++ deploy/terraform/terraform.ps1
@@ -1,10 +1,14 @@
-param ([bool] $apply = $false)
+param ([string] $github_ref)
 
 terraform init -no-color
 terraform workspace select test -no-color
 terraform init -no-color
 terraform validate -no-color
+terraform plan -no-color
 
-if($apply -eq $True) {
-    terraform plan -no-color
+if($github_ref -eq "refs/heads/master") {
+    Write-Host "Called from master branch, applying."
+    terraform apply -no-color -input=false -auto-approve
+} else {
+    Write-Host "Not called from master branch, not applying."
 }
