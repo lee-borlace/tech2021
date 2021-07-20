d8641b561d0f46a091f5e3d8e58695cd4f9b9a14
Lee Borlace <lee.borlace@gmail.com>
2021-07-20 20:00:54 +0800

    More ps logging

--- deploy/terraform/terraform.ps1
+++ deploy/terraform/terraform.ps1
@@ -1,14 +1,38 @@
 param ([string] $github_ref)
 
+Write-Host "***********************************************"
+Write-Host "Main init"
+Write-Host "***********************************************"
 terraform init -no-color
+
+Write-Host "***********************************************"
+Write-Host "Workspace select"
+Write-Host "***********************************************"
 terraform workspace select test -no-color
+
+Write-Host "***********************************************"
+Write-Host "Workspace init"
+Write-Host "***********************************************"
 terraform init -no-color
+
+Write-Host "***********************************************"
+Write-Host "Validate"
+Write-Host "***********************************************"
 terraform validate -no-color
+
+Write-Host "***********************************************"
+Write-Host "Plan"
+Write-Host "***********************************************"
 terraform plan -no-color
 
 if($github_ref -eq "refs/heads/master") {
+    Write-Host "***********************************************"
     Write-Host "Called from master branch, applying."
+    Write-Host "***********************************************"
+
     terraform apply -no-color -input=false -auto-approve
 } else {
+    Write-Host "***********************************************"
     Write-Host "Not called from master branch, not applying."
+    Write-Host "***********************************************"
 }
