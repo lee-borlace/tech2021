f8950bb74125943c9f583b2582b96a87cd50ddd6
Lee Borlace <lee.borlace@gmail.com>
2021-07-23 17:30:55 +0800

    build

--- build/buildCode.ps1
+++ build/buildCode.ps1
@@ -1,3 +1,8 @@
 Write-Host "***********************************************"
 Write-Host "Building code"
 Write-Host "***********************************************"
+
+Set-Location ../src/Leegle.ReactWebApp.Web/ClientApp
+npm install
+npm run build
+Set-Location ../../../build
